1c791400a951f6789d78628b2d6dbcd4d74e1aa6
Ackchyrin <86160552+Ackchyrin@users.noreply.github.com>
2022-03-21 21:08:35 +0500

    Add files via upload

--- styles/main.js
+++ styles/main.js
@@ -1,15 +1,26 @@
 let sendReviews = document.querySelector('.header__title-button')
 popupContainerReviews = document.querySelector('.reviews__popup-container')
 popupReviews = document.querySelector('.reviews__popup')
+popupCloseButton = document.querySelector('.reviews__popup-close')
 
 sendReviews.addEventListener('click', openReviewsPopup)
+popupCloseButton.addEventListener('click', closeReviewsPopup)
+popupContainerReviews.addEventListener('click', closeReviewsPopup)
 
 function openReviewsPopup(){
     popupContainerReviews.style.display = "block"
     popupReviews.style.display = "flex"
+    document.addEventListener('keydown', closePopupEsc);
 }
 
-function closeReviewsPopup{
+function closeReviewsPopup(){
     popupContainerReviews.style.display = "none"
     popupReviews.style.display = "none"
+    document.removeEventListener('keydown', closePopupEsc);
+}
+
+function closePopupEsc(event){
+    if (event.key === 'Escape') {
+        closeReviewsPopup();
+        }
 }
